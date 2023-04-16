a03e25153e65a334c13d797c589c91ef68a74c9b
diti85 <101580207+diti85@users.noreply.github.com>
2023-04-15 23:53:52 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,12 @@ import  {getUser} from './database.js'
 import { login } from './database.js'
 
 const app = express()
-const PORT = 8080 || 3000;
-app.use((err, req, res, next) => {
+app.use(express.json())
+const PORT = 8080 ;
+
+
+
+app.use( (err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
   })
@@ -20,8 +24,8 @@ app.get("/api/v1/:id", async (req,res) => {
 }) 
 
 app.post("/api/login", async (req, res) => {
-    const username = req.params.username 
-    const password = req.params.password 
+    const username = req.body.username 
+    const password = req.body.password 
     const user = await login(username, password) 
-    res.send(user)
+    res.status(200).send(user)
 })
