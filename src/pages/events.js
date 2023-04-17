4a6e892207e92358d6c51c41f298f543a2285b29
Jess0420 <53028568+Jess0420@users.noreply.github.com>
2023-04-17 19:35:56 -0400

    events added need to finish css
    event page added need to update front end

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,5 +1,6 @@
 import '../stylesheets/App.css'
-import '../stylesheets/home.css'
+import '../stylesheets/home.css' 
+import '../stylesheets/posts.css'
 import Navbar from '../components/navbar'; 
 import  Axios  from 'axios';
 import { useState } from 'react'; 
@@ -23,17 +24,25 @@ useEffect(() => {
 
 
 
-return (  
-    <div>
+  return (
+    
+    
+    <div className="post-container">
+    <h1 className='title'> Public Events</h1>
       {events.map(event => (
-        <div key={event.id}>
-          <h2>{event.title}</h2> 
-          <h2>{event.location_name}</h2>
-          <p>{event.description}</p>
-          <p>{event.date}</p>
+        <div className="post" key={event.id}>
+          <div className="post-header">
+            <h2>{event.name}</h2> 
+            <p>{event.date}</p>
+          </div>
+          <div className="post-body">
+            <h3>{event.location_name}</h3>
+            <p>{event.description}</p>
+          </div>
         </div>
       ))}
-    </div>
+    </div> 
+       
   );
 }
 export default Events;
