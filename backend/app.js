c084a2689474c5f06e13b059bfbf4b66a1b916d5
Endrit Basha <101580207+diti85@users.noreply.github.com>
2023-04-17 04:17:00 -0400

    Revert "merging develop with master (#3)"
    This reverts commit 6a1d9e3f5bfffb606047342a713af63fce2de2c7.

--- backend/app.js
+++ backend/app.js
@@ -32,26 +32,4 @@ app.post("/api/login", async (req, res) => {
       res.status(404).send();
     }
     res.status(200).send(user)
-    res.send(user)
-})
-
-
-app.post("/register", async (req, res) => {
-  const username = req.body.username 
-  const password = req.body.password 
-  const user = await login(username, password) 
-  res.send(user)
-})
-app.get("/api/publicevents", async (req,res) => {
-  const events = await getPublicEvents()
-  console.log(events);
-  res.status(201).send(events)
-}) 
-
-
-app.get("/api/userevents/:id",async(req,res)=>{
-  console.log(req.params.id)
-  const events = await getUserEvents(req.params.id)
-  console.log(events);
-  res.status(201).send(events)
 })
